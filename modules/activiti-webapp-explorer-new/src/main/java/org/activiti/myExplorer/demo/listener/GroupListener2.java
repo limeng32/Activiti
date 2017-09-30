0f7c4492c8f41eeec5f7a57f13eacc863d8589a3
limeng32 <limeng32@live.cn>
2017-09-30 16:26:52 +0800

    优化Spring配置文件，现在SIGroup类可以采用延迟单例的方式加载。

--- modules/activiti-webapp-explorer-new/src/main/java/org/activiti/myExplorer/demo/listener/GroupListener2.java
+++ modules/activiti-webapp-explorer-new/src/main/java/org/activiti/myExplorer/demo/listener/GroupListener2.java
@@ -1,16 +1,18 @@
 package org.activiti.myExplorer.demo.listener;
 
+import org.activiti.ApplicationContextProvider;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 import org.activiti.myExplorer.demo.model.SIGroup;
 
 public class GroupListener2 implements JavaDelegate {
 
-	private SIGroup siGroup = new SIGroup();
-
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		execution.setVariable("siGroup", siGroup);
+		execution.setVariable("siGroup", getSIGroup());
 	}
 
+	private SIGroup getSIGroup() {
+		return (SIGroup) ApplicationContextProvider.getApplicationContext().getBean(SIGroup.class);
+	}
 }
