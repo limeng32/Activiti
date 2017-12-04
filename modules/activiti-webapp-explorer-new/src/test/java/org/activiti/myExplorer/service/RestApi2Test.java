f696a260d93484848448ca356586213539c54435
limeng32 <limeng32@live.cn>
2017-12-04 12:11:27 +0800

    增加了平行网关相关的测试用例

--- modules/activiti-webapp-explorer-new/src/test/java/org/activiti/myExplorer/service/RestApi2Test.java
+++ modules/activiti-webapp-explorer-new/src/test/java/org/activiti/myExplorer/service/RestApi2Test.java
@@ -3,6 +3,7 @@ package org.activiti.myExplorer.service;
 import org.activiti.engine.RepositoryService;
 import org.activiti.explorer.conf.DemoDataConfiguration;
 import org.activiti.myExplorer.model.EndCode;
+import org.activiti.myExplorer.model.ExecutionReturn;
 import org.activiti.myExplorer.model.ProcessInstReturn;
 import org.activiti.myExplorer.model.RetCode;
 import org.activiti.myExplorer.web.CommonController;
@@ -23,7 +24,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import com.github.springtestdbunit.dataset.ReplacementDataSetLoader;;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -60,9 +66,9 @@ public class RestApi2Test {
 	}
 
 	@Test
-//	@DatabaseSetup(type = DatabaseOperation.CLEAN_INSERT, value = "/org/activiti/myExplorer/service/restApiTest/testJustStart.xml")
-//	@ExpectedDatabase(assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED, value = "/org/activiti/myExplorer/service/restApiTest/testJustStart.result.xml")
-//	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "/org/activiti/myExplorer/service/restApiTest/testJustStart.xml")
+	@DatabaseSetup(type = DatabaseOperation.CLEAN_INSERT, value = "/org/activiti/myExplorer/service/restApi2Test/testJustStart.xml")
+	@ExpectedDatabase(assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED, value = "/org/activiti/myExplorer/service/restApi2Test/testJustStart.result.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "/org/activiti/myExplorer/service/restApi2Test/testJustStart.xml")
 	public void testJustStart() throws Exception {
 		prepare();
 		ModelAndView modelAndView = this.mockMvc
@@ -72,5 +78,52 @@ public class RestApi2Test {
 		Assert.assertEquals(RetCode.success, processInstReturn.getRetCode());
 		Assert.assertEquals("1", processInstReturn.getRetVal());
 		Assert.assertEquals(EndCode.no, processInstReturn.getIsEnd());
+	}
+
+	@Test
+	@DatabaseSetup(type = DatabaseOperation.CLEAN_INSERT, value = "/org/activiti/myExplorer/service/restApi2Test/testFlow.xml")
+	@ExpectedDatabase(assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED, value = "/org/activiti/myExplorer/service/restApi2Test/testFlow.result.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "/org/activiti/myExplorer/service/restApi2Test/testFlow.xml")
+	public void testFlow() throws Exception {
+		prepare();
+		ModelAndView modelAndView = this.mockMvc.perform(MockMvcRequestBuilders.get("/start").param("businessId", "ttt")
+				.param("dealRole", "e").param("dealPerson", "dean")).andReturn().getModelAndView();
+		ProcessInstReturn processInstReturn = (ProcessInstReturn) modelAndView.getModelMap().get("_content");
+		ExecutionReturn[] ExecutionReturnA = processInstReturn.getExecutionReturn()
+				.toArray(new ExecutionReturn[processInstReturn.getExecutionReturn().size()]);
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/flow").param("exeId", ExecutionReturnA[0].getExeId())
+				.param("dealPerson", "dean").param("dealRole", "e")).andReturn().getModelAndView();
+	}
+
+	@Test
+	@DatabaseSetup(type = DatabaseOperation.CLEAN_INSERT, value = "/org/activiti/myExplorer/service/restApi2Test/testFlow2.xml")
+	@ExpectedDatabase(assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED, value = "/org/activiti/myExplorer/service/restApi2Test/testFlow2.result.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "/org/activiti/myExplorer/service/restApi2Test/testFlow2.xml")
+	public void testFlow2() throws Exception {
+		prepare();
+		ModelAndView modelAndView = this.mockMvc.perform(MockMvcRequestBuilders.get("/start").param("businessId", "ttt")
+				.param("dealRole", "e").param("dealPerson", "dean")).andReturn().getModelAndView();
+		ProcessInstReturn processInstReturn = (ProcessInstReturn) modelAndView.getModelMap().get("_content");
+		ExecutionReturn[] ExecutionReturnA = processInstReturn.getExecutionReturn()
+				.toArray(new ExecutionReturn[processInstReturn.getExecutionReturn().size()]);
+		modelAndView = this.mockMvc.perform(MockMvcRequestBuilders.get("/flow")
+				.param("exeId", ExecutionReturnA[0].getExeId()).param("dealPerson", "dean").param("dealRole", "e"))
+				.andReturn().getModelAndView();
+		processInstReturn = (ProcessInstReturn) modelAndView.getModelMap().get("_content");
+		ExecutionReturn[] ExecutionReturnB = processInstReturn.getExecutionReturn()
+				.toArray(new ExecutionReturn[processInstReturn.getExecutionReturn().size()]);
+
+		modelAndView = this.mockMvc.perform(MockMvcRequestBuilders.get("/flow")
+				.param("exeId", ExecutionReturnB[0].getExeId()).param("dealPerson", "dean").param("dealRole", "e"))
+				.andReturn().getModelAndView();
+		processInstReturn = (ProcessInstReturn) modelAndView.getModelMap().get("_content");
+		Assert.assertEquals(0, processInstReturn.getExecutionReturn().size());
+
+		modelAndView = this.mockMvc.perform(MockMvcRequestBuilders.get("/flow")
+				.param("exeId", ExecutionReturnB[1].getExeId()).param("dealPerson", "dean").param("dealRole", "e"))
+				.andReturn().getModelAndView();
+		modelAndView = this.mockMvc.perform(MockMvcRequestBuilders.get("/flow")
+				.param("exeId", ExecutionReturnB[1].getExeId()).param("dealPerson", "dean").param("dealRole", "e"))
+				.andReturn().getModelAndView();
 	}
 }
