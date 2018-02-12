3961af708d91760f57885e269486c4673bdb722c
limeng32 <limeng32@live.cn>
2018-02-12 16:04:07 +0800

    增加了activiti-remould-adapter模块，用来放置改造模块中的公用部分；

--- modules/activiti-account/src/main/java/org/activiti/account/persist/Account.java
+++ modules/activiti-account/src/main/java/org/activiti/account/persist/Account.java
@@ -1,5 +1,20 @@
 package org.activiti.account.persist;
 
-public class Account {
+import java.io.Serializable;
+
+import org.activiti.account.face.AccountFace;
+import org.activiti.myExplorer.pojoHelper.PojoSupport;
+
+import indi.mybatis.flying.annotations.TableMapperAnnotation;
+
+@TableMapperAnnotation(tableName = "account")
+public class Account extends PojoSupport<Account> implements Serializable, AccountFace {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Object getId() {
+		return null;
+	}
 
 }
--- modules/activiti-remould-adapter/src/main/java/org/activiti/myExplorer/pojoHelper/PojoSupport.java
+++ modules/activiti-remould-adapter/src/main/java/org/activiti/myExplorer/pojoHelper/PojoSupport.java
@@ -4,6 +4,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
+
 /**
  * @author 李萌
  * @date 2017年11月6日 上午11:15:04
