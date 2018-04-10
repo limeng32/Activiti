91c71cb89412f56fca695ff4000ab268a2d3eac4
limeng32 <limeng32@live.cn>
2018-04-10 18:56:14 +0800

    使用了druid数据源，并解决数据库无法重连的问题；

--- modules/activiti-webapp-explorer-new/src/main/java/org/activiti/explorer/conf/ApplicationConfiguration.java
+++ modules/activiti-webapp-explorer-new/src/main/java/org/activiti/explorer/conf/ApplicationConfiguration.java
@@ -17,8 +17,7 @@ import org.springframework.context.annotation.PropertySources;
 @PropertySources({ @PropertySource(value = "classpath:db.properties", ignoreResourceNotFound = true),
 		@PropertySource(value = "classpath:engine.properties", ignoreResourceNotFound = true) })
 @ComponentScan(basePackages = { "org.activiti.explorer.conf" })
-@ImportResource({ "classpath:activiti-ui-context.xml", "classpath:activiti-login-context.xml",
-		"classpath:activiti-custom-context.xml" })
+@ImportResource({ "classpath:activiti-ui-context.xml", "classpath:activiti-login-context.xml"})
 public class ApplicationConfiguration {
 
 }
